b7e5e243e3543a82815253cb68665a6166945f8d
Diego Bernardi <diegobernardisza@gmail.com>
2024-07-04 17:33:10 -0300

    add store produts models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Agua, Gas
+
+
+admin.site.register(Agua)
+admin.site.register(Gas)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
+from .models import Agua, Gas
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['list_aguas'] = Agua.objects.all()
+        context['list_gas'] = Gas.objects.all()
+        return context
