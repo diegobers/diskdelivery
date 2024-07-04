f3695a9f0473d0017ca93c64234de5bd3acf383f
Diego Bernardi <diegobernardisza@gmail.com>
2024-07-04 13:58:20 -0300

    initial index template

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path
-
-from django.views.generic.base import TemplateView
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', include('store.urls')),
 ]
 
 if settings.DEBUG:
