744b780e8d0950361d1db4e6d0407972dfb35bd8
Diego Bernardi <diegobernardisza@gmail.com>
2024-07-12 19:59:55 -0300

    add login for superuser

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+
 from . import views
 
-
 app_name = 'store'
+
 
 urlpatterns = [  
     path('', views.IndexView.as_view(), name='index'),
     path('adm/', views.AdminDashboardView.as_view(), name='admin_dashboard'),
-
+    path('entrar/', LoginView.as_view(template_name='layouts/admin-login.html'), name='login'),
 ]
