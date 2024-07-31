c54312cb7c46e6f475538dcd97fd984e78f36f65
Diego Bernardi <diegobernardisza@gmail.com>
2024-07-30 23:46:13 -0300

    add news config settings

--- core/settings.py
+++ core/settings.py
@@ -23,20 +23,38 @@ SECRET_KEY = env("ENV_SECRET_KEY")
 ALLOWED_HOSTS = env.list('HOSTS')
 
 CSRF_ALLOWED_ORIGINS = ['https://diskdelivery.up.railway.app']
-
 CSRF_TRUSTED_ORIGINS = ['https://diskdelivery.up.railway.app']
+CORS_ORIGINS_WHITELIST = ['https://diskdelivery.up.railway.app']
 
-# App's
+SITE_ID = 1
+
+INTERNAL_IPS = [
+    '127.0.0.1:8000',
+    'localhost:8000',
+]
+
+# Apps
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'django.contrib.sites',
+    
+    # my apps
+    'accounts',
     'cart',
     'checkout',
+    'store',
+
+    # third lib
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'django_bootstrap5',
 ]
 
@@ -49,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -81,6 +100,11 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Database
 DATABASES = { 
@@ -127,3 +151,82 @@ STATIC_URL = env.str("STATIC_URL", default="static/")
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Sessions
+#SESSION_COOKIE_AGE = 1209600 # two weeks in secs
+SESSION_COOKIE_NAME = 'disk'
+SESSION_COOKIE_SECURE = False # allow http request
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_SAVE_EVERY_REQUEST = False
+
+# Account settings
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'store:index'
+LOGOUT_REDIRECT_URL = 'store:index'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+AUTH_USER_MODEL = 'accounts.CustomUserModel'
+ACCOUNT_FORMS = {
+    'login': 'accounts.forms.CustomLoginForm',
+    'signup': 'accounts.forms.CustomSignupForm',
+}
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = LOGIN_REDIRECT_URL
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_CHANGE_EMAIL = False
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+#ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = settings.LOGIN_URL
+#ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = None -> settings.LOGIN_REDIRECT_URL
+#ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+#ACCOUNT_EMAIL_CONFIRMATION_HMAC = True
+ACCOUNT_EMAIL_NOTIFICATIONS = False
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'http://testSubject-prefix.com'
+ACCOUNT_EMAIL_UNKNOWN_ACCOUNTS = False
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+ACCOUNT_MAX_EMAIL_ADDRESSES = 1
+#ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = False
+ACCOUNT_LOGOUT_ON_GET = True
+#ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE =False
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
+ACCOUNT_LOGOUT_REDIRECT_URL = LOGOUT_REDIRECT_URL
+# --------->                                                 UNcheck
+ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = False
+ACCOUNT_PRESERVE_USERNAME_CASING = True
+#ACCOUNT_PREVENT_ENUMERATION = True
+ACCOUNT_REAUTHENTICATION_TIMEOUT = 300
+ACCOUNT_REAUTHENTICATION_REQUIRED = False
+ACCOUNT_SESSION_REMEMBER = None
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = False
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
+ACCOUNT_SIGNUP_REDIRECT_URL = LOGIN_REDIRECT_URL
+ACCOUNT_TEMPLATE_EXTENSION = 'html'
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USER_MODEL_EMAIL_FIELD = 'email'
+ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'
+ACCOUNT_USERNAME_MIN_LENGTH = 2
+ACCOUNT_USERNAME_REQUIRED = False
+
+# SocialAccount settings
+SOCIALACCOUNT_ADAPTER = 'cart.adapter.CustomSocialAccountAdapter'
+SOCIALACCOUNT_FORMS = {
+    'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
+    'signup': 'allauth.socialaccount.forms.SignupForm',
+}
+SOCIALACCOUNT_AUTO_SIGNUP = True
+SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
+SOCIALACCOUNT_LOGIN_ON_GET = True
+SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = True
+
+# SocialApp OAuth2 based provider
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,
+    }
+}
